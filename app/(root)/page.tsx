c900971b55b3f84fcd734e142800160705b40f33
karash <kappalaharshith@gmail.com>
2025-08-10 23:03:33 +0530

    home ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,19 @@
-
+import HeaderBox from "@/components/HeaderBox"
 const Home = () => {
+  const loggedin={firstname:'Karash'}
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedin?.firstname || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
