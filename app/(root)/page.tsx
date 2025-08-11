a430005267d37c8d24d16f924378066138955d88
karash <kappalaharshith@gmail.com>
2025-08-11 23:58:38 +0530

    home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from "@/components/HeaderBox"
+import TotakBalanceBox from "@/components/TotakBalanceBox"
 const Home = () => {
   const loggedin={firstname:'Karash'}
   return (
@@ -7,10 +8,16 @@ const Home = () => {
         <header className="home-header">
           <HeaderBox
             type="greeting"
-            title="welcome"
+            title="W  elcome"
             user={loggedin?.firstname || "Guest"}
             subtext="Access and manage your account and transactions efficiently"
           />
+
+          <TotakBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+            />
         </header>
       </div>
     </section>
